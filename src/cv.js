36739a2639ab1cf49bfd9b979f6c7ec4c54cdace
arhik <karthik.katipalli@gmail.com>
2016-05-01 23:19:06 -0400

    resume changes

--- src/cv.js
+++ src/cv.js
@@ -1,11 +1,18 @@
 import {bindable} from "aurelia-framework"
 export class CV{
-	@bindable python
+	@bindable python;
+	@bindable pythonExp;
+	@bindable tabmenu;
 	constructor(){
 		
 	}
 	attached(){
-		console.log($(this.python))
-		$(this.python).progress('increment')
+		$(this.python).progress({percent:90});
+		$(this.C).progress({percent:75});
+		$(this.cpp).progress({percent:85});
+		$(this.java).progress({percent:75});
+		$(this.go).progress({percent:75});
+		$(this.js).progress({percent:85});
+		$(this.tabmenu).children().tab();
 	}
 }
