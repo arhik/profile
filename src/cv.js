2232805496feb1874d81948a950e5362784ee32e
arhik <karthik.katipalli@gmail.com>
2016-05-01 20:54:27 -0400

    updated resume

--- src/cv.js
+++ src/cv.js
@@ -1,7 +1,11 @@
+import {bindable} from "aurelia-framework"
 export class CV{
+	@bindable python
 	constructor(){
 		
 	}
-
-	
+	attached(){
+		console.log($(this.python))
+		$(this.python).progress('increment')
+	}
 }
