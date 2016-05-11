c277954733cc8fe625c9710f38821bf28d7d66bc
arhik <karthik.katipalli@gmail.com>
2016-05-10 20:03:53 -0400

    test database motor

--- motor_test.py
+++ motor_test.py
@@ -8,23 +8,37 @@ import os
 import motor.motor_tornado
 import pprint
 import json
-db = motor.motor_tornado.MotorClient().test_database
+from bson import json_util
+db = motor.motor_tornado.MotorClient().arhik
 
-class MainHandler(tornado.web.RequestHandler):
+
+class BlogsHandler(tornado.web.RequestHandler):
     async def get(self):
         db = self.settings['db']
-        result = await db.movie.insert({"01":"Highway"})
-        list = db.movie.find()
-        async for document in db.movie.find():
-            pprint.pprint(document)
-        self.write({'id': repr(result)})
-    async def post(self):
-        db = self.settings['db']
-        data = self.request.body
-        result = await db.user.insert({"data":data})
-        print(result)
+        blogs = db.blogs.find()
+        self.set_header("Content-Type", "application/json")
+        async for blog in blogs:
+            self.write(json.dumps(blog,default=json_util.default))
+        # self.write(str(blogs))
+        # self.finish()
+        
+class MainHandler(tornado.web.RequestHandler):
+    pass
+    # async def get(self):
+    #     db = self.settings['db']
+    #     result = await db.movie.insert({"01":"Highway"})
+    #     list = db.movie.find()
+    #     async for document in db.movie.find():
+    #         pprint.pprint(document)
+    #     self.write({'id': repr(result)})
+    # async def post(self):
+    #     db = self.settings['db']
+    #     data = self.request.body
+    #     result = await db.user.insert({"data":data})
+    #     print(result)
 application = tornado.web.Application([
-    (r'/', MainHandler)
+    (r'/', MainHandler),
+    (r'/blogs', BlogsHandler)
 ], db=db)
 
 application.listen(8888)
