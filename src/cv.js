4b1b153adce400abee3252c9b48543538198b277
arhik <karthik.katipalli@gmail.com>
2016-05-10 20:03:25 -0400

    experience on large projects update

--- src/cv.js
+++ src/cv.js
@@ -13,6 +13,12 @@ export class CV{
 		$(this.java).progress({percent:75});
 		$(this.go).progress({percent:75});
 		$(this.js).progress({percent:85});
+		$(this.pythonExp).progress({percent:90});
+		$(this.CExp).progress({percent:65});
+		$(this.cppExp).progress({percent:75});
+		$(this.javaExp).progress({percent:65});
+		$(this.goExp).progress({percent:85});
+		$(this.jsExp).progress({percent:90});
 		$(this.tabmenu).children().tab();
 	}
 }
